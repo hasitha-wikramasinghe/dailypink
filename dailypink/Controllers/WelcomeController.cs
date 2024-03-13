7d309984cc2ff4a5ec9dfb67c0dd0c02133df8a5
Hasitha Wikramasinghe <hasitha.softworxio@gmail.com>
2024-03-14 01:24:03 +0530

    Create ProductViewModel Server side implementations

--- dailypink/Controllers/WelcomeController.cs
+++ dailypink/Controllers/WelcomeController.cs
@@ -1,11 +1,31 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using dailypink.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace dailypink.Controllers
 {
     public class WelcomeController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _context;
+        public WelcomeController(
+            ApplicationDbContext context)
         {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await (from product in _context.Products
+                           join category in _context.Categories
+                           on product.Id equals category.Id into PCJoin
+                           from category in PCJoin.DefaultIfEmpty()
+                           select new ProductViewModel()
+                           {
+                               Id = product.Id,
+                               Name = product.Name, 
+                               Description = product.Description,
+                               CategoryName = category.Name,
+                           }).ToListAsync();
+
             return View();
         }
     }

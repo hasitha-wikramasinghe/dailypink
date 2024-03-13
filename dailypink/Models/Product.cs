a36ad15b6d7058c3cadeb757f1513da729923a80
Hasitha Wikramasinghe <78442062+hasitha-wikramasinghe@users.noreply.github.com>
2024-03-14 00:55:25 +0530

    Conclude welcome page ui (#2)
    Co-authored-by: Hasitha Wikramasinghe <hasitha.softworxio@gmail.com>

---
+++ dailypink/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace dailypink.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public string? ImageURL { get; set; }
+    }
+}
